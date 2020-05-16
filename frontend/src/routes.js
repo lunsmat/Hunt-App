0734278be9be4b1adbebd55dac3ef06cfd182ebf
Luan Farias <matheus.frs22123@outlook.com>
2020-05-15 23:19:14 -0300

    Frontend of Application

---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Product from './pages/Product';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route exact path="/products/:id" component={Product} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
